84c5835750a549373f3e7c2b141e006f6d59ec49
Katja Haferkorn <katjahafi@gmx.de>
2021-03-19 10:52:49 +0100

    Expo text

---
+++ 2021/Expo/index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+	<?php include "../inc/meta.inc"; ?>
+
+	<base href="<?php echo dirname($_SERVER['SCRIPT_NAME']); ?>">
+	<title>FOSSGIS 2021 - Expo</title>
+
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/base.css">
+	<link rel="stylesheet" href="./css/print.css" media="print">
+</head>
+
+<body id="expo">
+
+    <?php include "../inc/header.inc"; ?>
+
+    <h2>Ausstellerstand auf der FOSSGIS</h2>
+
+    <p>Die bewährte Firmen- und Projektausstellung, die einen direkten Kontakt zwischen Dienstleistern, Projekten und Teilnehmer:innen bietet, wird in diesem Jahr in einem besonderen Format stattfinden. Dazu werden wir in der Netzwerkwelt einen Expobereich einrichten.</p>
+	<p>Im Expobereich können sich Firmen und Open Source Projekte präsentieren</p>
+	<p>Jeder Ausstellungsstand wird mit dem eignen Logo gekennzeichnet, bekommt einen eignen Videokonferenzraum sowie eine Infoseite, die per Mouseover angezeigt wird. In der Netzwerkwelt, die den OST Campus Rapperswil nachbilden wird, können sich die Teilnehmenden in einer virtuellen Karte/Landschaft bewegen und einander begegnen. 
+Zum Einstieg gibt man sich einen Namen und wählt einen Avatar aus. Nach der Landung im Eingangsbereich können Sie sich mit Hilfe der Tastatur bewegen. Sie können die Netzwerkwelt erkunden, sehen auch, ob weitere Teilnehmende unterwegs sind.<br>
+Bei Begegnung von zwei Avataren öffnet sich ein Videochat, welcher sich mit einem kurzen Geräusch ankündigt. Räume können betreten werden, dort sind Informationen zu finden oder eine Videokonferenz findet statt. Das heißt Sie, als Aussteller, können die Teilnehmenden sehen und auch aktiv auf diese zugehen.<br>
+Der Ausstellerstand ist im Sponsoringpaket der FOSSGIS-Konferenz enthalten und bis zum 10.04.2021 kostenfrei buchbar. </p>
+
+  
+	<h4 class="highlight">Liste der Aussteller</h4>
+				<ul class="tiles">
+					<li class="tile expo">
+						<a href="Link1" target="_blank">
+						<img src="./img/s/Logo1.jpg" alt="Firma1">
+						</a>
+					</li>
+				<li class="tile expo">
+						<p> Firmenbezeichnung1 <br>
+							Kurztext der Firma1 </p>
+					</li>
+				<li class="tile expo">
+						<p> Link zur Ausstellerseite1</p>
+					</li>
+				</ul>
+				<ul class="tiles">
+					<li class="tile expo">
+						<a href="Link2" target="_blank">
+						<img src="./img/s/Logo2.jpg" alt="Firma2">
+						</a>
+					</li>
+				<li class="tile expo">
+						<p> Firmenbezeichnung2 <br>
+							Kurztext der Firma2 </p>
+					</li>
+				<li class="tile expo">
+						<p> Link zur Ausstellerseite2</p>
+					</li>
+				</ul>
+    
+    
+ 
+	
+    <?php include('../inc/footer.inc'); ?>
+</body>
+
+</html>
